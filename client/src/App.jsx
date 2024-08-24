d367e8f848d0ff359b5dda69fc75187eaf7f0b1c
BUCH WARREN <warren.buch@etu.unistra.fr>
2024-08-24 22:15:28 +0200

    Commencement page home

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Home from './home/Home'
+
+export default function App() {
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
---
+++ client/src/home/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home(){
+    return (
+        <div id="home-wrapper">
+            <div id="home">
+                <h1>Xxx<span>quiz</span></h1>
+                <h2>This is an adult website</h2>
+                <p>This website contains age-restricted materials including nudity and  
+                explicit depictions of sexual activity. By entering, you affirm that you  are at 
+                least 18 years of age or the age of majority in the jurisdiction  you are accessing
+                the website from and you consent to viewing sexually  explicit content.</p>
+            </div>
+        </div>
+    )
+}
